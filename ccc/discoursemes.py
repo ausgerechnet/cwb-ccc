694d4210e72760fd5f549ceeb95f1a4f9006dc52
Philipp Heinrich <philipp.heinrich@fau.de>
2020-07-19 18:36:24 +0200

    several fixes in discoursemes submodule

--- ccc/discoursemes.py
+++ ccc/discoursemes.py
@@ -4,7 +4,6 @@
 # part of module
 from ccc.utils import formulate_cqp_query
 from ccc.utils import merge_intervals
-from ccc.utils import concordance_line2df
 from ccc.collocates import df_node_to_cooc, add_ams
 # requirements
 from pandas import merge, DataFrame
@@ -14,15 +13,35 @@ logger = logging.getLogger(__name__)
 
 
 class Disc:
+    """
+    realization of a discourseme given a corpus and query parameters
+    """
 
     def __init__(self, corpus, items, p_query, s_query, s_context,
-                 context=20, flags="%cd", escape=False, name=None):
+                 context=20, flags="%cd", escape=False):
+        """
+        .corpus
+        .items
 
-        # TODO: start independent CQP process?
+        .parameters:
+          p_query
+          s_query
+          s_context
+          context
+          flags
+          escape
+
+        .dump
+        .idx
+        ._context = None
+        ._matches = None
+        """
+
+        # TODO: start independent CQP process
         self.corpus = corpus
+        self.items = items
 
         self.parameters = {
-            'items': items,
             'p_query': p_query,
             's_query': s_query,
             's_context': s_context,
@@ -36,11 +55,6 @@ class Disc:
         self.dump, self.idx = corpus.query(
             query, context, s_context=s_context, return_name=True
         )
-
-        # give the child a name
-        self.name = name
-        if self.name is None:
-            self.name = self.idx
 
         self._context = None
         self._matches = None
@@ -64,9 +78,12 @@ class Disc:
 
         return self._context
 
-    def show_concordance(self, context=20, matches=None, p_show=['word'],
-                         s_show=[], p_text=None, p_slots=None, regions=[],
-                         order='first', cut_off=100, form='kwic'):
+    def show_concordance(self, context=None, matches=None,
+                         p_show=['word'], s_show=[], order='random',
+                         cut_off=100, form='dataframes'):
+
+        if context is None:
+            context = self.parameters['context']
 
         if context > self.parameters['context']:
             logger.warning(
@@ -86,10 +103,11 @@ class Disc:
         else:
             df = self.dump
 
+        # TODO: start independent CQP process
         conc = self.corpus.concordance(df)
         return conc.lines(
             matches=matches, p_show=p_show, s_show=s_show,
-            p_text=p_text, p_slots=p_slots, regions=regions, order=order,
+            p_text=None, p_slots=None, regions=[], order=order,
             cut_off=cut_off, form=form
         )
 
@@ -97,6 +115,7 @@ class Disc:
                         p_query="lemma", ams=None, min_freq=2,
                         frequencies=True, flags=None):
 
+        # TODO: start independent CQP process
         coll = self.corpus.collocates(self.dump, p_query)
         return coll.show(
             window=window, order=order, cut_off=cut_off, ams=ams,
@@ -105,27 +124,66 @@ class Disc:
 
 
 class DiscPos:
+    """
+    realization of a discursive position given a topic and discoursemes
+    discoursemes can be added after initialization
+    """
 
-    def __init__(self, topic, corpus, discoursemes):
+    def __init__(self, topic, discoursemes=[], name='Last'):
+        """
+        .topic
+        .corpus
+        .parameters
+        .name
+
+        .discoursemes: dict of Discs with key == Disc.idx
+
+        .df_nodes = dict of df_nodes with key == window
+        """
 
         self.topic = topic
-        self.discoursemes = discoursemes
-        self.df_nodes = DataFrame()
-        self.corpus = corpus
+
+        # TODO: start independent CQP process
+        self.corpus = topic.corpus
+        self.parameters = topic.parameters
+        self.name = name
+
+        self.discoursemes = dict()
+        for d in discoursemes:
+            self.add_disc(d)
+
+        self.df_nodes = dict()
 
     def add_disc(self, disc):
-        self.discoursemes.append(disc)
 
-    def slice_discs(self, context=None):
+        if disc.idx in self.discoursemes.keys():
+            logger.warning("discourseme already associated to discursive position")
+        else:
+            self.discoursemes[disc.idx] = disc
+            self.df_nodes = dict()  # reset df nodes
+
+    def add_items(self, items):
+        # TODO: start independent CQP process
+        disc = Disc(
+            corpus=self.corpus,
+            items=items,
+            **self.parameters
+        )
+        self.add_disc(disc)
+        return disc.idx
+
+    def slice_discs(self, window=None):
         """ intersects all discoursemes
         :return: df_nodes with topic match, matchend,
-        context_id, context, contextend, match_disc_1, matchend_disc_1, ...
+        context_id, context, contextend, match_disc1_idx, matchend_disc1_idx, ...
         """
-        if context is None:
-            context = self.topic.parameters['context']
+
+        if window is None:
+            window = self.topic.parameters['context']
 
         df_nodes = self.topic.dump.reset_index()
-        for disc in self.discoursemes:
+
+        for disc in self.discoursemes.values():
             # get dump
             df = disc.dump.reset_index()
             df = df.drop(['context', 'contextend'], axis=1)
@@ -134,67 +192,78 @@ class DiscPos:
             # remove lines where items are too far away
             df_nodes['offset'] = df_nodes.apply(calculate_offset, axis=1)
             df_nodes = df_nodes[
-                abs(df_nodes['offset']) <= context
+                abs(df_nodes['offset']) <= window
             ]
             df_nodes = df_nodes.rename(columns={
                     'match_x': 'match',
                     'matchend_x': 'matchend',
-                    'match_y': 'match_' + disc.name,
-                    'matchend_y': 'matchend_' + disc.name,
-                    'offset': 'offset_' + disc.name
+                    'match_y': 'match_' + disc.idx,
+                    'matchend_y': 'matchend_' + disc.idx,
+                    'offset': 'offset_' + disc.idx
             })
 
-        self.df_nodes = df_nodes
+        self.df_nodes[window] = df_nodes
+
         return df_nodes
 
-    def show_concordance(self, context=20, matches=None,
-                         p_show=['word'], order='random', cut_off=100):
+    def show_concordance(self, window=5, matches=None,
+                         p_show=['word'], s_show=[], order='random',
+                         cut_off=100, form='dataframes'):
 
         """ self.df_nodes has duplicate entries
         (1) convert to (match matchend) disc_1_set disc_2_set ...
         (2) convert each line to dataframe
         """
 
-        self.slice_discs(context)  # make sure we're having the right context
+        # make sure we're having the right context
+        if window not in self.df_nodes.keys():
+            df_nodes = self.slice_discs(window).copy()
+        else:
+            df_nodes = self.df_nodes[window]
+
+        # get ids of all discoursemes
+        disc_ids = set(self.discoursemes.keys())
 
         logger.info("converting discourse nodes to regular dump")
-        matches = set(self.df_nodes['match'])
+        all_matches = set(df_nodes['match'])
         rows = list()
-        for match in matches:
+        for match in all_matches:
             row = dict()
-            df_loc = self.df_nodes.loc[self.df_nodes.match == match]
-            disc_names = [
-                c.split("offset_")[1] for c in df_loc.columns if c.startswith("offset_")
-            ]
+            df_loc = df_nodes.loc[df_nodes.match == match]
             row['match'] = match
             row['matchend'] = df_loc.iloc[0]['matchend']
             row['context_id'] = df_loc.iloc[0]['context_id']
             row['context'] = df_loc.iloc[0]['context']
             row['contextend'] = df_loc.iloc[0]['contextend']
-            for disc in disc_names:
+            for idx in disc_ids:
                 disc_f1 = set()
-                for a, b in zip(df_loc['match_' + disc], df_loc['matchend_' + disc]):
+                for a, b in zip(df_loc['match_' + idx], df_loc['matchend_' + idx]):
                     disc_f1.update(range(a, b + 1))
-                row[disc] = disc_f1
+                row[idx] = disc_f1
             rows.append(row)
         df = DataFrame(rows)
         df = df.set_index(["match", "matchend"])
 
         logger.info("converting each line to dataframe")
+        # TODO: start independent CQP process
         conc = self.corpus.concordance(df)
-        lines = conc.lines(matches=matches, p_show=p_show, order=order,
-                           cut_off=cut_off, form='dataframes')
+        lines = conc.lines(
+            matches=matches, p_show=p_show, s_show=s_show,
+            p_text=None, p_slots=None, regions=[], order=order,
+            cut_off=cut_off, form=form
+        )
 
-        logger.info("inserting discourseme info")
+        logger.info("inserting discourseme and window/context info")
+        # TODO mark out of context
         dfs = list()
         for line in lines.iterrows():
             df = line[1]['df']
             # indicate topic matches
             match, matchend = line[0]
-            df[self.topic.name] = df.index.isin(set(range(match, matchend + 1)))
+            df[self.topic.idx] = df.index.isin(set(range(match, matchend + 1)))
             # indicate discourseme matches
-            for disc in disc_names:
-                df[disc] = df.index.isin(line[1][disc])
+            for idx in disc_ids:
+                df[idx] = df.index.isin(line[1][idx])
             df = df.drop(['match', 'matchend', 'context', 'contextend'], axis=1)
             dfs.append(df)
         lines['df'] = dfs
@@ -205,13 +274,20 @@ class DiscPos:
                         p_query="lemma", ams=None, min_freq=2,
                         frequencies=True, flags=None):
 
-        self.slice_discs(window)  # make sure we're having the right context
+        # make sure we're having the right context
+        if window not in self.df_nodes.keys():
+            df_nodes = self.slice_discs(window)
+        else:
+            df_nodes = self.df_nodes[window]
 
         # get and correct df_cooc, f1_set
-        df_cooc, f1_set = df_node_to_cooc(self.df_nodes)
-        for disc in self.discoursemes:
-            logger.info('excluding discourseme "%s" from context' % disc.name)
-            matches = disc.matches()
+        df_cooc, f1_set = df_node_to_cooc(df_nodes)
+        if len(f1_set) == 0:
+            logger.warning("no matches")
+            return DataFrame()
+        for idx in self.discoursemes:
+            logger.info('excluding discourseme "%s" from context' % idx)
+            matches = self.discoursemes[idx].matches()
             f1_set.update(matches)
             df_cooc = df_cooc.loc[~df_cooc['cpos'].isin(f1_set)]
 
@@ -220,6 +296,7 @@ class DiscPos:
         # number of possible occurrence positions within window
         f1_inflated = len(relevant)
 
+        # TODO: start independent CQP process
         # get frequency counts
         f = self.corpus.counts.cpos(
             relevant['cpos'], [p_query]
