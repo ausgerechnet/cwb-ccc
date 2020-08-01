8d6c72671ba1a050e7b138f65dd9c36af172f5ce
Philipp Heinrich <philipp.heinrich@fau.de>
2020-08-01 03:47:28 +0200

    improve utils

--- ccc/utils.py
+++ ccc/utils.py
@@ -7,13 +7,29 @@ from functools import wraps
 # requirements
 import numpy as np
 from unidecode import unidecode
-from pandas import DataFrame
 # logging
 import logging
 logger = logging.getLogger(__name__)
 
 
-# split list in tuples (for anchor queris)
+def time_it(func):
+    """
+    decorator for printing the execution time of a function call
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = default_timer()
+        result = func(*args, **kwargs)
+        end = default_timer()
+        name = func.__name__
+        logger.info("%s took %s seconds" % (name, round(end - start, 2)))
+        return result
+    return wrapper
+
+
+###########
+# ANCHORS #
+###########
 def chunk_anchors(lst, n, exclude={0, 1}):
     """yields n-sized chunks from lst without excluded values"""
     lst_rm = [i for i in lst if i not in exclude]
@@ -21,7 +37,6 @@ def chunk_anchors(lst, n, exclude={0, 1}):
         yield lst_rm[i:i + n]
 
 
-# anchor corrections
 def apply_correction(row, correction):
     """Applies correction to one row in column.
 
@@ -54,7 +69,9 @@ def correct_anchors(df, corrections):
     return df
 
 
-# query processing
+###########
+# QUERIES #
+###########
 def cqp_escape(token):
     """ escape CQP meta-characters
 
@@ -64,8 +81,7 @@ def cqp_escape(token):
     :rtype: str
 
     """
-
-    escaped = token.translate(str.maketrans({
+    return token.translate(str.maketrans({
         ".": r"\.",
         "?": r"\?",
         "*": r"\*",
@@ -81,12 +97,14 @@ def cqp_escape(token):
         "$": r"\$"
     }))
 
-    return escaped
-
 
 def formulate_cqp_query(items, p_query='word', s_query=None,
                         flags="", escape=True):
-    """ wrapper for easy queries """
+    """ wrapper for easy queries
+
+    :return: valid cqp query
+    :rtype: str
+    """
 
     # gather all MWUs
     mwu_queries = list()
@@ -123,9 +141,9 @@ def formulate_cqp_query(items, p_query='word', s_query=None,
 
 
 def preprocess_query(query):
-    """ parse anchors and within statement from query """
+    """ parse anchors and 'within' statement from query """
 
-    # get s_query and strip within statement
+    # get s_query and strip 'within' statement
     s_query = None
     query = query.rstrip(";")
     tokens = query.split(" ")
@@ -144,21 +162,9 @@ def preprocess_query(query):
     return query, s_query, anchors
 
 
-def time_it(func):
-    """
-    decorator for printing the execution time of a function call
-    """
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start = default_timer()
-        result = func(*args, **kwargs)
-        end = default_timer()
-        name = func.__name__
-        logger.info("%s took %s seconds" % (name, round(end - start, 2)))
-        return result
-    return wrapper
-
-
+#################################
+# working on nodes and contexts #
+#################################
 def node2cooc(row):
     """ convert one row of df_node to info for df_cooc """
 
@@ -192,7 +198,51 @@ def node2cooc(row):
     return result
 
 
-# s-att handling
+def merge_intervals(inter, start_index=0):
+    """ for merging contexts """
+    for i in range(start_index, len(inter)-1):
+        if inter[i][1] >= inter[i+1][0]:
+            new_start = inter[i][0]
+            new_end = inter[i+1][1]
+            inter[i] = [new_start, new_end]
+            del inter[i+1]
+            return merge_intervals(inter.copy(), start_index=i)
+    return inter
+
+
+def calculate_offset(row):
+    """ calculates offset of y to x """
+
+    # necessary values
+    match_x = row['match_x']
+    match_y = row['match_y']
+
+    # init matchends if necessary
+    if 'matchend_x' in row.keys():
+        matchend_x = row['matchend_x']
+    else:
+        matchend_x = match_x
+    if 'matchend_y' in row.keys():
+        matchend_y = row['matchend_y']
+    else:
+        matchend_y = match_y
+
+    # y ... x
+    if match_x > matchend_y:
+        offset = matchend_y - match_x
+    # x ... y
+    elif matchend_x < match_y:
+        offset = matchend_x - match_y
+    # overlap
+    else:
+        offset = 0
+
+    return offset
+
+
+##################
+# s-att handling #
+##################
 def merge_s_atts(s_query, s_break, s_meta):
     """ consistencize s-atts """
     # s_query < s_break < s_meta
@@ -230,18 +280,21 @@ def merge_s_atts(s_query, s_break, s_meta):
     return s_query, s_break, s_meta
 
 
+##################
+# p-att handling #
+##################
 def fold_item(item, flags="%cd"):
-    # TODO align with CWB
     if flags is None:
         return item
-
-    if "d" in flags:
-        # remove diacritica
-        item = unidecode(item)
 
     if "c" in flags:
         # lower-case
         item = item.lower()
+
+    if "d" in flags:
+        # TODO align with CWB
+        # remove diacritica
+        item = unidecode(item)
 
     return item
 
@@ -251,15 +304,3 @@ def fold_df(df, flags="%cd"):
     grouped = df.groupby(df.index)
     df = grouped.aggregate(np.sum)
     return df
-
-
-def merge_intervals(inter, start_index=0):
-    """ for merging contexts """
-    for i in range(start_index, len(inter)-1):
-        if inter[i][1] >= inter[i+1][0]:
-            new_start = inter[i][0]
-            new_end = inter[i+1][1]
-            inter[i] = [new_start, new_end]
-            del inter[i+1]
-            return merge_intervals(inter.copy(), start_index=i)
-    return inter
