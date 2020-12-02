c95be8ab4948148741a38f60b25b366586a2f172
Philipp Heinrich <philipp.heinrich@fau.de>
2020-12-03 00:52:59 +0100

    improve readability

--- ccc/ufa.py
+++ ccc/ufa.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 class UFA:
 
-    def __init__(self, corpus, ids, context_break='text_id'):
+    def __init__(self, corpus, s_dict, s_att='text_id'):
         """
         :param Corpus corpus: corpus to work on
         :param dict ids: dicitonary of {subcorpus_name: set of values for context_break}
@@ -14,22 +14,22 @@ class UFA:
         """
 
         self.corpus = corpus.copy()
-        self.ids = ids
-        self.context_break = context_break
+        self.s_dict = s_dict
+        self.s_att = s_att
 
         logger.info("creating subcorpora ...")
-        subcorpora = dict()
+        dumps = dict()
         i = 0
-        for s in self.ids.keys():
+        for s in self.s_dict.keys():
             i += 1
-            logger.info("... subcorpus %d of %d" % (i, len(self.ids)))
-            subcorpora[s] = corpus.dump_from_s_att(
-                self.context_break, self.ids[s]
+            logger.info("... subcorpus %d of %d" % (i, len(self.s_dict)))
+            dumps[s] = corpus.dump_from_s_att(
+                self.s_att, self.s_dict[s]
             )
 
-        self.subcorpora = subcorpora
+        self.dumps = dumps
 
-    def keywords(self, p_query='lemma', order='f', cut_off=100,
+    def keywords(self, p_query='lemma', order='log_likelihood', cut_off=100,
                  ams=None, min_freq=2, frequencies=True, flags=None,
                  subset=None):
         """
@@ -37,7 +37,7 @@ class UFA:
         :rtype: dict
         """
         if subset is None:
-            subset = list(self.ids.keys())
+            subset = list(self.s_dict.keys())
 
         logger.info("computing keyword tables ...")
         tables = dict()
@@ -45,7 +45,7 @@ class UFA:
         for s in subset:
             i += 1
             logger.info("... table %d of %d" % (i, len(subset)))
-            dump = self.subcorpora[s]
+            dump = self.dumps[s]
             tables[s] = dump.keywords(
                 p_query=p_query, order=order, cut_off=cut_off,
                 ams=ams, min_freq=min_freq,
@@ -55,14 +55,14 @@ class UFA:
         return tables
 
     def collocates(self, cqp_query, window=5, p_query='lemma',
-                   order='f', cut_off=100, ams=None, min_freq=2,
+                   order='log_likelihood', cut_off=100, ams=None, min_freq=2,
                    frequencies=True, flags=None, subset=None):
         """
         :return: dictionary of {subcorpus_name: table}
         :rtype: dict
         """
         if subset is None:
-            subset = list(self.ids.keys())
+            subset = list(self.s_dict.keys())
 
         tables = dict()
 
@@ -70,11 +70,11 @@ class UFA:
         dump_glob = self.corpus.query(
             cqp_query,
             context=window,
-            context_break=self.context_break
+            context_break=self.s_att
         )
         df_glob = dump_glob.df.join(
             self.corpus.get_s_annotations(
-                dump_glob.df, [self.context_break]
+                dump_glob.df, [self.s_att]
             )
         )
 
@@ -84,14 +84,14 @@ class UFA:
         for s in subset:
             i += 1
             logger.info("... table %d of %d" % (i, len(subset)))
-            dump = self.subcorpora[s]
+            dump = self.dumps[s]
             # get local marginals (f2)
             marginals_loc = self.corpus.counts.dump(
                 dump.df, split=True, p_atts=[p_query]
             )
             # create collocates table
             df_loc = df_glob.loc[
-                df_glob[self.context_break].isin(self.ids[s])
+                df_glob[self.s_att].isin(self.s_dict[s])
             ]
             collocates = Collocates(self.corpus, df_loc, p_query=p_query)
             tables[s] = collocates.show(
