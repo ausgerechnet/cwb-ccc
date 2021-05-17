3d28af3c78d8b6fc606a6a0806e957949c747b41
Philipp Heinrich <philipp.heinrich@fau.de>
2021-05-18 00:09:37 +0200

    create cache directory if not exists

--- ccc/cache.py
+++ ccc/cache.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import shelve
+import os
 from hashlib import sha256
 # logging
 import logging
@@ -11,7 +12,12 @@ logger = logging.getLogger(__name__)
 class Cache:
 
     def __init__(self, path=None):
+
         self.path = path
+
+        if path:
+            directory = os.path.dirname(path)
+            os.makedirs(directory, exist_ok=True)
 
     def generate_idx(self, identifiers, prefix='', length=10):
         string = ''.join([str(idx) for idx in identifiers])
