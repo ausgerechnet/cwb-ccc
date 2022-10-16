41147cb48dd7e981f66efd9c9f9a2fbdfce1d2e4
Philipp Heinrich <philipp.heinrich@fau.de>
2022-10-16 21:27:12 +0200

    some more PEP compliance

--- ccc/cache.py
+++ ccc/cache.py
@@ -15,25 +15,29 @@ logger = logging.getLogger(__name__)
 
 
 def generate_idx(identifiers, prefix='', length=10):
+    """generate an ID from an iterable
 
+    """
     string = ''.join([str(idx) for idx in identifiers])
     identifier = sha256(str(string).encode()).hexdigest()
     return prefix + identifier[:length]
 
 
 def generate_library_idx(lib_path, prefix='', length=10):
+    """generate an ID wordlists and macros (subdirectories of lib_path)
 
+    """
     wordlists = glob(os.path.join(lib_path, 'wordlists', '*.txt'))
     macros = glob(os.path.join(lib_path, 'macros', '*.txt'))
     paths = macros + wordlists
     identifiers = [
-        generate_idx(open(p, 'rt')) for p in sorted(paths)
+        generate_idx(open(p, 'rt', encoding='utf-8')) for p in sorted(paths)
     ]
     return generate_idx(identifiers, prefix, length)
 
 
 class Cache:
-
+    
     def __init__(self, path=None):
 
         self.path = path
@@ -73,8 +77,6 @@ class Cache:
             if key in shelf.keys():
                 logger.info(f'retrieving object "{key}" from cache')
                 return shelf[key]
-            else:
-                return None
 
     def set(self, identifier, value):
 
