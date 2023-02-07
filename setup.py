b1d73703efcec8842fe3cd2c4cde0c6c646846cf
Philipp Heinrich <philipp.heinrich@fau.de>
2023-02-07 14:01:38 +0100

    clean up setup.py

--- setup.py
+++ setup.py
@@ -17,13 +17,12 @@ USE_CYTHON = False              # use cython -2 ccc/cl.pyx instead
 ########################
 # read local resources #
 ########################
+
 here = os.path.abspath(os.path.dirname(__file__))
 
-# description
 with open(os.path.join(here, 'README.md'), mode='rt', encoding='utf-8') as f:
     long_description = f.read()
 
-# version
 version = {}
 with open(os.path.join(here, 'ccc', 'version.py'), mode='rt', encoding='utf-8') as f:
     exec(f.read(), version)
@@ -32,7 +31,10 @@ with open(os.path.join(here, 'ccc', 'version.py'), mode='rt', encoding='utf-8') 
 ##############
 # cwb-config #
 ##############
+
+# - version
 cwb_version = subprocess.run(shlex.split("cwb-config -v"), capture_output=True).stdout.decode().strip()
+# - cqp binaries
 cwb_bindir = subprocess.run(shlex.split("cwb-config --bindir"), capture_output=True).stdout.decode().strip()
 # - effective registry directory or directories:
 cwb_registry = subprocess.run(shlex.split("cwb-config -r"), capture_output=True).stdout.decode().strip()
@@ -53,6 +55,8 @@ cwb_linker_flags = subprocess.run(shlex.split("cwb-config -L"), capture_output=T
 # ensure compatibility with CWB v3.4.36 and below
 if int(cwb_version.split(".")[0]) == 3 and int(cwb_version.split(".")[1]) == 4 and int(cwb_version.split(".")[2]) < 37:
     cwb_linker_flags = "-L/usr/local/lib -lcl  -lm   -lpcre -lglib-2.0"
+
+# define include directories, library directories, and library names
 libraries = [t[2:] for t in shlex.split(cwb_linker_flags) if t.startswith("-l")]
 inc_dirs = [cwb_incdir] + [t[2:] for t in shlex.split(cwb_compiler_flags) if t.startswith("-I")]
 lib_dirs = [cwb_libdir] + [t[2:] for t in shlex.split(cwb_linker_flags) if t.startswith("-L")]
@@ -62,7 +66,7 @@ ccc_cl = Extension(
     sources=['ccc/cl' + ('.pyx' if USE_CYTHON else '.c')],
     include_dirs=inc_dirs,  # list of directories to search for C/C++ header files
     library_dirs=lib_dirs,  # list of directories to search for C/C++ libraries at link time
-    libraries=libraries         # list of library names (not filenames or paths) to link against
+    libraries=libraries     # list of library names (not filenames or paths) to link against
 )
 
 # cythonize?
@@ -104,10 +108,10 @@ setup(
         "Development Status :: 4 - Beta",
         "Operating System :: Unix",
         "Programming Language :: Python :: 3",
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Cython',
     ],
 )
