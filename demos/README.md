d761596b3ebf6dcad963c1068d001e04ef6b52e9
Philipp Heinrich <philipp.heinrich@fau.de>
2025-03-10 10:58:01 +0100

    clean up demo folder

---
+++ demos/README.md
@@ -0,0 +1,5 @@
+# cwb-ccc demos
+
+- files here provide examples on how to use cwb-ccc
+- they are based on historic versions of cwb-ccc
+- scripts might not run in the most recent version
--- demos/readme.py
+++ demos/readme.py
@@ -1,12 +1,10 @@
 """readme.py: create tables for the README.md"""
 
-import sys
 
 import pyperclip
 
-sys.path.append("/home/ausgerechnet/implementation/cwb-ccc/")
 import ccc
-from ccc import Corpora, Corpus
+from ccc import Corpus
 
 print(ccc.__version__)
 
@@ -57,3 +55,19 @@ copymd(dump.concordance(form='kwic', order='random'))
 copymd(dump.collocates())
 dump = corpus.query(s_query='text_party', s_values={'CDU', 'CSU'})
 copymd(dump.keywords(order='conservative_log_ratio'))
+
+
+# Test = [lemma="haben"] @0[xpos="P.*"] @1[lemma=".*"];
+# group Test target lemma by keyword lemma
+# NB: target = 0; keyword = 1
+
+query = '@0[pos="N.*"] [lemma="sein"] [pos="ART.*"]? @1[pos="N.*"];'
+germaparl = Corpus("GERMAPARL1386", registry_path="tests/corpora/registry/")
+
+dump = germaparl.query(query)
+conc = dump.concordance(p_show=['lemma'], form='slots', cut_off=None)
+conc.groupby(['1_lemma', '0_lemma']).size().sort_values(ascending=False)
+
+cqp = germaparl.start_cqp()
+cqp.Query(f'Test={query}')
+print(cqp.Group("Test target lemma by keyword lemma;"))
