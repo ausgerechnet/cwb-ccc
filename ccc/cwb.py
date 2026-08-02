47b951551bb26e1f813d8a23d70f9565bf693222
Philipp Heinrich <philipp.heinrich@fau.de>
2026-08-02 20:33:36 +0200

    basic support for aligned corpora (fix issue #85)

--- ccc/cwb.py
+++ ccc/cwb.py
@@ -8,6 +8,7 @@ definition of the Corpora, Corpus and SubCorpus classes
 import logging
 import os
 from io import StringIO
+from itertools import groupby
 
 # requirements
 from numpy import maximum, minimum
@@ -451,6 +452,74 @@ class Corpus:
         df = df.sort_values(by=['freq', 'item'], ascending=False)
 
         return df
+
+    ################
+    # a-attributes #
+    ################
+    def cpos2alg(self, cpos, a_att):
+        """Get alignment attribute at cpos. -1 if not present.
+
+        :param int cpos: corpus position
+
+        :return: alignment attribute
+        :rtype: int
+
+        """
+        a_attributes = self.attributes.attribute(a_att, "a")
+        try:
+            return a_attributes.cpos2alg(cpos)
+        except KeyError:
+            return -1
+
+    def _dump2aatt_row(self, row, a_att):
+        """Retrieve a-attribute annotation from a row."""
+
+        cpos_start = row.get("match", -1)
+        cpos_end = row.get("matchend", -1)
+
+        # Empty row
+        if cpos_start == cpos_end == -1:
+            return (-1, -1, -1, -1)
+
+        if cpos_start == -1:
+            cpos_start = cpos_end
+        if cpos_end == -1:
+            cpos_end = cpos_start
+
+        a_attr = self.attributes.attribute(a_att, "a")
+
+        values = [
+            a_attr[value]
+            for value, _ in groupby(
+                self.cpos2alg(cpos, a_att)
+                for cpos in range(int(cpos_start), int(cpos_end) + 1)
+            )
+        ]
+
+        if not values:
+            return (-1, -1, -1, -1)
+
+        return (
+            min(value[0] for value in values),  # start_a
+            max(value[1] for value in values),  # end_a
+            min(value[2] for value in values),  # start_b
+            max(value[3] for value in values),  # end_b
+        )
+
+    def dump2aatt(self, df_dump, a_att):
+        """Retrieve a-attribute annotation for each row."""
+
+        results = df_dump.reset_index().apply(
+            self._dump2aatt_row,
+            axis=1,
+            args=(a_att,),
+        )
+
+        return DataFrame(
+            results.tolist(),
+            columns=["start_a", "end_a", "start_b", "end_b"],
+            index=results.index,
+        )
 
     ################
     # s-attributes #
@@ -1554,6 +1623,7 @@ class SubCorpus(Corpus):
         return df
 
     def concordance(self, form='simple', p_show=['word'], s_show=[],
+                    a_show=[],
                     order='first', cut_off=100, matches=None,
                     slots=None, cwb_ids=False):
 
@@ -1562,7 +1632,7 @@ class SubCorpus(Corpus):
             df_dump=self.df
         )
 
-        return conc.lines(
+        lines = conc.lines(
             form=form,
             p_show=p_show,
             s_show=s_show,
@@ -1572,6 +1642,28 @@ class SubCorpus(Corpus):
             slots=slots,
             cwb_ids=cwb_ids
         )
+
+        # join aligned versions
+        for a_att in a_show:
+            alignments = self.dump2aatt(self.df, a_att).\
+                rename({'start_b': 'match', 'end_b': 'matchend'}, axis=1).\
+                set_index(['match', 'matchend'])
+            aligned = SubCorpus(
+                subcorpus_name=None,
+                df_dump=alignments,
+                corpus_name=a_att.upper(),
+                lib_dir=self.lib_dir,
+                cqp_bin=self.cqp_bin,
+                registry_dir=self.registry_dir,
+                data_dir=None,
+                overwrite=True,
+                inval_cache=True
+
+            )
+            for p_att in p_show:
+                lines[a_att + '_' + p_att] = aligned.dump2patt(alignments, p_att=p_att)[p_att].reset_index(drop=True).tolist()
+
+        return lines
 
     def collocates(self, p_query=['lemma'], mws=20, window=5,
                    order='O11', cut_off=100, ams=None, min_freq=2,
--- tests/test_13_alignment.py
+++ tests/test_13_alignment.py
@@ -2,7 +2,16 @@ from ccc import Corpus
 from .conftest import DATA_PATH
 
 
-def test_alignment(holmes_en):
+def test_alignment_attribute(holmes_de, holmes_en):
+
+    corpus = Corpus(
+        holmes_de['corpus_name'],
+        registry_dir=holmes_en['registry_dir'],
+        lib_dir=None,
+        data_dir=DATA_PATH
+    )
+
+    assert holmes_en['corpus_name'].lower() in corpus.available_attributes()['attribute'].to_list()
 
     corpus = Corpus(
         holmes_en['corpus_name'],
@@ -11,4 +20,42 @@ def test_alignment(holmes_en):
         data_dir=DATA_PATH
     )
 
-    print(corpus.available_attributes())
+    assert holmes_de['corpus_name'].lower() in corpus.available_attributes()['attribute'].to_list()
+
+
+def test_alignment_dump2aatt(holmes_de, holmes_en):
+
+    src = Corpus(
+        holmes_de['corpus_name'],
+        registry_dir=holmes_de['registry_dir'],
+        lib_dir=None,
+        data_dir=DATA_PATH
+    )
+
+    tgt = holmes_en['corpus_name']
+    tgt_attribute = tgt.lower()
+
+    matches = src.query('[lemma="ich"]')
+    matches.cpos2alg(44, tgt_attribute)
+    a = matches.dump2aatt(matches.df, tgt_attribute)
+    assert len(a) == 6
+
+
+def test_alignment_concordancing(holmes_de, holmes_en):
+
+    src = Corpus(
+        holmes_de['corpus_name'],
+        registry_dir=holmes_de['registry_dir'],
+        lib_dir=None,
+        data_dir=DATA_PATH
+    )
+
+    tgt = holmes_en['corpus_name']
+    tgt_attribute = tgt.lower()
+
+    matches = src.query('[lemma="ich"]', context=5)
+    result = matches.concordance(a_show=[tgt_attribute],
+                                 p_show=['word', 'lemma'])
+
+    assert 'holmes-en_word' in result.columns
+    assert 'holmes-en_lemma' in result.columns
